6e56f0b93b28a79957576f2aefa3e7b571968b8f
nmdmn <afarago.nmd@gmail.com>
2026-03-16 13:08:19 +0100

    ADD basic app skeleton
    Following the tutorial, the very basic HelloTriangleApplication.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,40 @@
 #include "config.h"
+#include <exception>
+#include <stdexcept>
+#include <vulkan/vulkan.h>
 #include <iostream>
+#include <cstdlib>
+#include <sstream>
 
-int main(int argc, char *argv[]) { std::cout << NJIN_NAME << std::endl; }
+class HelloTriangleApplication {
+public:
+  void run() {
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initVulkan() { throw std::invalid_argument("Some error occured ~!@#$%^&*()_+"); }
+
+  void mainLoop() {}
+
+  void cleanup() {}
+};
+
+int main() {
+  std::stringstream ss;
+  ss << NJIN_NAME << " v" << NJIN_VERSION;
+  std::cout << ss.str() << std::endl;
+
+  HelloTriangleApplication app;
+
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
