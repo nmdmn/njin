c3650e51f0e09ac23647ba0b75881a08e77610c6
nmdmn <afarago.nmd@gmail.com>
2026-03-16 15:47:47 +0100

    ADD window initialization
    Also set c++20.

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,55 @@
-#include "config.h"
 #include <exception>
-#include <stdexcept>
-#include <vulkan/vulkan.h>
 #include <iostream>
-#include <cstdlib>
-#include <sstream>
 
-class HelloTriangleApplication {
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "config.h"
+
+// TODO Proper RAII! c-tor/d-tor usage - implement via unique_ptr/shared_ptr with custom deleter!
+
+namespace njin {
+class App {
 public:
   void run() {
+    std::cout << NJIN_NAME << " v" << NJIN_VERSION << std::endl;
+
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
-  void initVulkan() { throw std::invalid_argument("Some error occured ~!@#$%^&*()_+"); }
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    _window = glfwCreateWindow(WIDTH, HEIGHT, NJIN_NAME, nullptr, nullptr);
+  }
 
-  void mainLoop() {}
+  void initVulkan() {}
 
-  void cleanup() {}
+  void mainLoop() {
+    while (!glfwWindowShouldClose(_window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(_window);
+    glfwTerminate();
+  }
+
+  static constexpr uint32_t WIDTH = 800;
+  static constexpr uint32_t HEIGHT = 600;
+
+  GLFWwindow *_window;
 };
+} // namespace njin
 
 int main() {
-  std::stringstream ss;
-  ss << NJIN_NAME << " v" << NJIN_VERSION;
-  std::cout << ss.str() << std::endl;
-
-  HelloTriangleApplication app;
+  njin::App app;
 
   try {
     app.run();
