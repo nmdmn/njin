23935fe66d3991abac910be29832f722a013fdb6
nmdmn <afarago.nmd@gmail.com>
2026-03-21 17:59:44 +0100

    ADD vulkan instance creation
    ...failing ofc!

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <exception>
+#include <vulkan/vulkan_core.h>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -30,7 +31,10 @@ private:
     window_ = glfwCreateWindow(WIDTH, HEIGHT, NJIN_NAME, nullptr, nullptr);
   }
 
-  void initVulkan() {}
+  void initVulkan() {
+    createInstance();
+    // TODO ...
+  }
 
   void mainLoop() {
     while (!glfwWindowShouldClose(window_)) {
@@ -43,10 +47,36 @@ private:
     glfwTerminate();
   }
 
-  static constexpr int WIDTH = 800;
-  static constexpr int HEIGHT = 600;
+  void createInstance() {
+    VkApplicationInfo app_info{};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = NJIN_NAME;
+    app_info.applicationVersion = VK_MAKE_VERSION(NJIN_VERSION_MAJOR, NJIN_VERSION_MINOR, NJIN_VERSION_PATCH);
+    app_info.pEngineName = "No Engine";
+    app_info.engineVersion = VK_MAKE_VERSION(NJIN_VERSION_MAJOR, NJIN_VERSION_MINOR, NJIN_VERSION_PATCH);
+    app_info.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo create_info{};
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    create_info.pApplicationInfo = &app_info;
+
+    uint32_t glfw_extension_count = 0;
+    const char **glfw_extensions;
+    glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+    create_info.enabledExtensionCount = glfw_extension_count;
+    create_info.ppEnabledExtensionNames = glfw_extensions;
+    create_info.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&create_info, nullptr, &instance_) != VK_SUCCESS) {
+      throw std::runtime_error(" Vulkan instance creation failed!");
+    }
+  }
+
+  static constexpr uint32_t WIDTH = 800;
+  static constexpr uint32_t HEIGHT = 600;
 
   GLFWwindow *window_;
+  VkInstance instance_;
 };
 } // namespace njin
 
@@ -56,7 +86,7 @@ int main() {
   try {
     app.run();
   } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
+    njin::Logger::instance().error << e.what();
     return EXIT_FAILURE;
   }
 
