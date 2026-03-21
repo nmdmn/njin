b2367b7cb5c12b68b81b814eea41fa18f2c8a845
nmdmn <afarago.nmd@gmail.com>
2026-03-21 11:41:42 +0100

    ADD multi-threaded logger

---
+++ src/logger.hh
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <format>
+#include <chrono>
+
+#include "config.hh"
+
+#ifndef NJIN_LOGGER
+#define NJIN_LOGGER 3
+#endif
+
+namespace njin {
+
+class Logger {
+public:
+  enum class Level { TRACE = 1, INFO = 2, WARNING = 3, ERROR = 4 };
+
+  struct Null {
+    template <typename T> Null &operator<<(const T &) { return *this; }
+  };
+
+  struct Stream {
+    Stream(Level level, Logger &logger)
+        : level_(level)
+        , logger_(logger) {}
+
+    ~Stream() {
+      if (moved_)
+        return;
+      logger_.log(level_, string_stream_.str());
+    }
+
+    Stream(Stream &&other)
+        : level_(other.level_)
+        , logger_(other.logger_)
+        , string_stream_(std::move(other.string_stream_)) {
+      other.moved_ = true;
+    }
+
+    Stream(const Stream &) = delete;
+    Stream &operator=(const Stream &) = delete;
+
+    template <typename T> Stream &operator<<(const T &msg) {
+      string_stream_ << msg;
+      return *this;
+    }
+
+    Level level_;
+    Logger &logger_;
+    bool moved_;
+    std::ostringstream string_stream_;
+  };
+
+  template <Level L> struct Proxy {
+    template <typename T> auto operator<<(const T &msg) const {
+      if constexpr (static_cast<int>(L) >= NJIN_LOGGER) {
+        Stream stream(L, logger_);
+        stream << msg;
+        return stream;
+      } else {
+        return Null{};
+      }
+    }
+
+    Logger &logger_;
+  };
+
+  static Logger &instance() {
+    static Logger logger;
+    return logger;
+  }
+
+  Logger(const Logger &) = delete;
+  Logger &operator=(const Logger &) = delete;
+  Logger(Logger &&) = delete;
+  Logger &operator=(Logger &&) = delete;
+
+  Proxy<Level::TRACE> trace{*this};
+  Proxy<Level::INFO> info{*this};
+  Proxy<Level::WARNING> warning{*this};
+  Proxy<Level::ERROR> error{*this};
+
+private:
+  static constexpr std::string_view level_str(Level level) {
+    switch (level) {
+    case Level::TRACE:
+      return "TRACE";
+    case Level::INFO:
+      return "INFO";
+    case Level::WARNING:
+      return "WARNING";
+    case Level::ERROR:
+      return "ERROR";
+    }
+  }
+
+  Logger() { worker_ = std::thread(&Logger::process, this); }
+  ~Logger() {
+    {
+      std::unique_lock<std::mutex> lock(mutex_);
+      drained_cond_var_.wait(lock, [this] { return queue_.empty(); });
+    }
+    is_running_.store(false);
+    cond_var_.notify_one();
+    worker_.join();
+  }
+
+  void process() {
+    do {
+      decltype(queue_) local;
+      {
+        std::unique_lock<std::mutex> lock(mutex_);
+        cond_var_.wait(lock, [this] { return !queue_.empty() || !is_running_; });
+        std::swap(local, queue_);
+      }
+      while (!local.empty()) {
+        std::cout << local.front() << std::endl;
+        local.pop();
+      }
+      drained_cond_var_.notify_one();
+    } while (is_running_);
+  }
+
+  void log(Level level, std::string_view msg) {
+    auto now = std::chrono::system_clock::now();
+    std::string entry = std::format("[{:%Y-%m-%d %H:%M:%S}] [{}] {}", std::chrono::floor<std::chrono::seconds>(now),
+                                    level_str(level), msg);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      queue_.push(std::string(entry));
+    }
+    cond_var_.notify_one();
+  }
+
+  std::queue<std::string> queue_;
+  std::thread worker_;
+  std::mutex mutex_;
+  std::condition_variable cond_var_;
+  std::condition_variable drained_cond_var_;
+  std::atomic<bool> is_running_ = true;
+};
+} // namespace njin
--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,13 @@
-#include <exception>
 #include <iostream>
+#include <thread>
+#include <string>
+#include <exception>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include "config.hh"
+#include "logger.hh"
 
 // TODO Proper RAII! c-tor/d-tor usage - implement via unique_ptr/shared_ptr with custom deleter!
 
@@ -12,7 +15,8 @@ namespace njin {
 class App {
 public:
   void run() {
-    std::cout << NJIN_NAME << " v" << NJIN_VERSION << std::endl;
+    auto &logger = Logger::instance();
+    logger.info << NJIN_NAME << " v" << NJIN_VERSION;
 
     initWindow();
     initVulkan();
@@ -25,28 +29,44 @@ private:
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    _window = glfwCreateWindow(WIDTH, HEIGHT, NJIN_NAME, nullptr, nullptr);
+    window_ = glfwCreateWindow(WIDTH, HEIGHT, NJIN_NAME, nullptr, nullptr);
   }
 
   void initVulkan() {}
 
   void mainLoop() {
-    while (!glfwWindowShouldClose(_window)) {
+    while (!glfwWindowShouldClose(window_)) {
       glfwPollEvents();
     }
   }
 
   void cleanup() {
-    glfwDestroyWindow(_window);
+    glfwDestroyWindow(window_);
     glfwTerminate();
   }
 
   static constexpr uint32_t WIDTH = 800;
   static constexpr uint32_t HEIGHT = 600;
 
-  GLFWwindow *_window;
+  GLFWwindow *window_;
 };
 } // namespace njin
+
+void test_logger() {
+  auto &logger = njin::Logger::instance();
+  auto worker = [&](int id) {
+    for (int i = 0; i < 5; ++i)
+      logger.trace << "thread " << std::to_string(id) << " msg " << std::to_string(i);
+  };
+
+  std::thread t1(worker, 1);
+  std::thread t2(worker, 2);
+  std::thread t3(worker, 3);
+
+  t1.join();
+  t2.join();
+  t3.join();
+}
 
 int main() {
   njin::App app;
